f293e047d07c5cb9efc428d9d9cd46f9cdad0e05
Engin Arslan <hibernationtheory@gmail.com>
2016-01-17 21:15:02 -0500

    refactor to use casperjs

---
+++ getPageNetworkData.js
@@ -0,0 +1,150 @@
+function main(url, callback) {
+	var RESOURCES = [];
+	var RESOURCES_FAILED = [];
+	var TARGET_ARRAYS = {
+		"html":[],
+		"javascript":[],
+		"css":[],
+		"image":[],
+		"json":[],
+		"svg_xml":[],
+		"font":[]
+	};
+	var END = false;
+
+	/*
+		Event Listeners
+	*/
+
+	casper.on('error', function(msg) {
+		console.log(msg);
+	});
+
+	casper.on('remote.message', function(msg) {
+		console.log(msg);
+	});
+
+	casper.on('resource.received', function(resource) {
+		if (resource.stage === 'end' && !END) {
+			RESOURCES.push(resource);
+			if (resource.status >= 400) {
+				RESOURCES_FAILED.push(resource);
+			}
+			organizeByContentType(resource);
+		}
+	});
+
+	/*
+		Utility Functions
+	*/
+
+	function calculateTotalContentSize(resourceArray) {
+		var total = 0;
+		resourceArray.forEach(function(item) {
+			var size = item["_contentLength"];
+			if (size && size > 0) {
+				total += size;
+			}
+		});
+		return total;
+	}
+
+	function promoteAndSortContentLengthData(resourceArray) {
+		var contentLengthAttr = "_contentLength";
+
+		resourceArray.forEach(function(item) {
+			var status = item.status;
+			var contentLength = -1;
+			item.headers.forEach(function(header) {
+				if (header.name === 'Content-Length' || header.name === 'content-length')  {
+					contentLength = parseInt(header.value, 10);
+				}
+			});
+			item[contentLengthAttr] = item[contentLengthAttr] || contentLength;
+		});
+
+		resourceArray.sort(function(item1, item2) {
+			var size1 = item1["_contentLength"];
+			var size2 = item2["_contentLength"];
+			return size1 - size2;
+		});
+	};
+
+	function organizeByContentType(resource) {
+		// gets a html response and categorizes it into an array according to the content
+
+		if (resource.contentType.indexOf('html') > -1) {
+			TARGET_ARRAYS['html'].push(resource);
+		} else if (resource.contentType.indexOf('javascript') > -1) {
+			TARGET_ARRAYS['javascript'].push(resource);
+		} else if (resource.contentType.indexOf('css') > -1) {
+			TARGET_ARRAYS['css'].push(resource);
+		} else if (resource.contentType.indexOf('svg+xml') > -1) {
+			TARGET_ARRAYS['svg_xml'].push(resource);
+		} else if (resource.contentType.indexOf('font') > -1) {
+			TARGET_ARRAYS['font'].push(resource);
+		} else if (resource.contentType.indexOf('json') > -1) {
+			TARGET_ARRAYS['json'].push(resource);
+		} else if (resource.contentType.indexOf('image') > -1) {
+			TARGET_ARRAYS['image'].push(resource);
+		}
+	};
+
+	function getPageNetworkData(url, callback) {
+		casper.thenOpen(url, function() {
+			var results = {
+				"resources":
+					{
+						"amount":0,
+						"smallest":"",
+						"largest":"",
+						"failed_ids":[],
+						"javascript":TARGET_ARRAYS['javascript'],
+						"css":TARGET_ARRAYS['css'],
+						"html":TARGET_ARRAYS['html'],
+						"json":TARGET_ARRAYS['json'],
+						"image":TARGET_ARRAYS['image'],
+						"all":[],
+						"size_javascript":0,
+						"size_css":0,
+						"size_html":0,
+						"size_json":0,
+						"size_image":0,
+						"size_all":0
+					}
+			};
+			var res_res = results["resources"];
+			promoteAndSortContentLengthData(RESOURCES);
+			res_res["amount"] = RESOURCES.length;
+			res_res["largest"] = RESOURCES.slice(-1)[0];
+			res_res["smallest"] = RESOURCES.filter(function(item) { if (item["_contentLength"] > 0 ) { return item }})[0];
+			res_res["all"] = RESOURCES;
+			res_res["size_all"] = calculateTotalContentSize(RESOURCES);
+
+			res_res["html"] = TARGET_ARRAYS['html'];
+			res_res["size_html"] = calculateTotalContentSize(res_res["html"]);
+			res_res["css"] = TARGET_ARRAYS['css'];
+			res_res["size_css"] = calculateTotalContentSize(res_res["css"]);
+			res_res["json"] = TARGET_ARRAYS['json'];
+			res_res["size_json"] = calculateTotalContentSize(res_res["json"]);
+			res_res["javascript"] = TARGET_ARRAYS['javascript'];
+			res_res["size_javascript"] = calculateTotalContentSize(res_res["javascript"]);
+			res_res["image"] = TARGET_ARRAYS['image'];
+			res_res["size_image"] = calculateTotalContentSize(res_res["image"]);
+
+			RESOURCES_FAILED.forEach(function(i) {
+				res_res["failed_ids"].push(i.id);
+				console.log('Resource failed to load: ' + i.url + ' with a ' + i.status);
+			});
+			END = true; // stop resource collection. would be better if I can just kill the event listener
+			callback(results);
+		});
+		casper.run(function() {
+		});
+
+		
+	}
+	getPageNetworkData(url, callback);
+};
+
+module.exports = main;
--- main.js
+++ main.js
@@ -1,186 +1,81 @@
-/* https://github.com/ariya/phantomjs/blob/master/examples/netsniff.js */
+/*
+	MODULES
+*/
+var casper = require('casper').create({
+	logLevel:'debug',
+	verbose:false
+});
+var fs = require('fs');
+var getPageNetworkData = require('./getPageNetworkData.js');
 
-var page = require('webpage').create();
-var system = require('system');
-var resources = [];
-var resourcesSucceeded = [];
-var resourcesFailed = [];
-// contentType
-//text/html
-// application/javascript
-//text/css
-//image/svg+xml
-//application/x-font-ttf 
-//application/json
-var TARGET_ARRAYS = {
-	"html":[],
-	"javascript":[],
-	"css":[],
-	"image":[],
-	"json":[],
-	"svg_xml":[],
-	"font":[]
+/*
+	GLOBALS & CONFIG
+*/
+
+var DATA_ALL = {};
+var URL_DATA_ALL = [];
+var COUNTER_REPEAT = 0;
+
+var config_file = './config.json';
+var config_data_json = fs.read(config_file);
+var config_data = JSON.parse(config_data_json);
+
+var URL_ALL = config_data["urls"]
+var TARGET_FILE = config_data["output_file"]
+
+function getMinMax(dataArray, attr) {
+	dataArray.sort(function(item1, item2) {
+		var size1 = item1.data.resources[attr];
+		var size2 = item2.data.resources[attr];
+		return size1 - size2;
+	});
+	return [dataArray[0].data.resources[attr], dataArray[dataArray.length-1].data.resources[attr]];
 };
 
-function organizeByContentType(resource) {
-	if (resource.contentType.indexOf('html') > -1) {
-		TARGET_ARRAYS['html'].push(resource);
-	} else if (resource.contentType.indexOf('javascript') > -1) {
-		TARGET_ARRAYS['javascript'].push(resource);
-	} else if (resource.contentType.indexOf('css') > -1) {
-		TARGET_ARRAYS['css'].push(resource);
-	} else if (resource.contentType.indexOf('svg+xml') > -1) {
-		TARGET_ARRAYS['svg_xml'].push(resource);
-	} else if (resource.contentType.indexOf('font') > -1) {
-		TARGET_ARRAYS['font'].push(resource);
-	} else if (resource.contentType.indexOf('json') > -1) {
-		TARGET_ARRAYS['json'].push(resource);
-	}
-};
-
-function sortContentData() {
-	/* not working */
-	for (var data in TARGET_ARRAYS) {
-		console.log(TARGET_ARRAYS[data].length);
-		TARGET_ARRAYS[data] = TARGET_ARRAYS[data].sort(function(item1, item2) {
-			var size1 = item1.bodySize || 0;
-			var size2 = item2.bodySize || 0;
-			return size1-size2;
-		});
-	};
-};
-
-function calculateTotalContentSize(resourceArray) {
-	var total = 0;
-	resourceArray.forEach(function(item) {
-		var size = item.bodySize;
-		if (size) {
-			total += size;
-		}
-	});
-	return total;
+function delayedLoop(maxLoops) {
+	var counter = 0;
+	(function next() {
+	    if (counter > maxLoops) {
+			end();
+			return;
+	    }
+	    
+	    setTimeout(function() {
+	    	casper.start();
+	        console.log(counter);
+	        repeat(counter);
+	        counter++;
+	        casper.wait(1000, next);
+	    }, 2000);
+	})();
 }
 
-function checkMinSize(resourceArray) {
-	var prevBodySize = null;
-	var prevFoundItem = null;
-	resourceArray.forEach(function(item) {
-		var status = item.status;
-		if (status < 400) {
-			var bodySize = item.bodySize;
-			if (bodySize && prevBodySize === null) {
-				prevBodySize = bodySize;
-			}
-			if (bodySize && bodySize < prevBodySize) {
-				prevBodySize = bodySize;
-				prevFoundItem = item;
-			}
-		}
-	});
-	return prevFoundItem;
-}
-
-function checkMaxSize(resourceArray) {
-	var prevBodySize = null;
-	var prevFoundItem = null;
-	resourceArray.forEach(function(item) {
-		var status = item.status;
-		if (status < 400) {
-			var bodySize = item.bodySize;
-			if (bodySize && prevBodySize === null) {
-				prevBodySize = bodySize;
-			}
-			if (bodySize && bodySize > prevBodySize) {
-				prevBodySize = bodySize;
-				prevFoundItem = item;
-			}
-		}
-	});
-	return prevFoundItem;
-}
-
-page.onError = function(msg) {
-	console.log(msg);
-}
-
-page.onConsoleMessage = function(msg) {
-	console.log(msg);
- }
-
-page.onResourceRequested = function(request) {
-	//console.log( JSON.stringify(request, undefined, 4) );
- }
-
-page.onResourceReceived = function(resource) {
-	resources.push(resource);
-	if (resource.status >= 400) {
-		resourcesFailed.push(resource);
-	} else {
-		resourcesSucceeded.push(resource);
-	}
-	organizeByContentType(resource);
- }
-
-function getPageNetworkData(url, callback) {
-	var results = {
-		"resources":
-			{
-				"amount":0,
-				"smallest":"",
-				"largest":"",
-				"succeeded":[],
-				"failed":[],
-				"javascript":TARGET_ARRAYS['js'],
-				"css":TARGET_ARRAYS['css'],
-				"html":TARGET_ARRAYS['html'],
-				"json":TARGET_ARRAYS['json'],
-				"all":[],
-				"size_javascript":0,
-				"size_css":0,
-				"size_html":0,
-				"size_json":0,
-				"size_all":0
-			}
-	};
-	var res_res = results["resources"];
-	page.open(url, function(status) {
-		if (status !== 'success') {
-			console.log('failed to load the address');
-			phantom.exit();
-		}
-		var largestResource = checkMaxSize(resources);
-		var smallestResource  = checkMinSize(resources);
-		res_res["amount"] = resources.length;
-		res_res["smallest"] = smallestResource;
-		res_res["largest"] = largestResource;
-		res_res["succeeded"] = resourcesSucceeded;
-		res_res["all"] = resources;
-		res_res["size_all"] = calculateTotalContentSize(resources);
-
-		sortContentData();
-		res_res["html"] = TARGET_ARRAYS['html'];
-		res_res["size_html"] = calculateTotalContentSize(res_res["html"]);
-		res_res["css"] = TARGET_ARRAYS['css'];
-		res_res["size_css"] = calculateTotalContentSize(res_res["css"]);
-		res_res["json"] = TARGET_ARRAYS['json'];
-		res_res["size_json"] = calculateTotalContentSize(res_res["json"]);
-		res_res["javascript"] = TARGET_ARRAYS['javascript'];
-		res_res["size_javascript"] = calculateTotalContentSize(res_res["javascript"]);
-
-
-		resourcesFailed.forEach(function(resource) {
-			res_res["failed"].push(resource);
-			console.log('Resource failed to load: ' + resource.url + ' with a ' + resource.status);
-		});
-
-		var resultsStr = JSON.stringify(results, null, '\t');
-		callback(resultsStr);
-		phantom.exit();
+function repeat(index) {
+	console.log('repeat');
+	url = URL_ALL[index];
+	getPageNetworkData(url, function(data) {
+		var currentData = {};
+		currentData["url"] = url;
+		currentData["data"] = data;
+		currentData["_id"] = index;
+		URL_DATA_ALL.push(currentData);
 	});
 }
 
-function main(url, callback) {
-	getPageNetworkData(url, callback);
-}
+function end() {
+	console.log('end');
+	var amountMinMax = getMinMax(URL_DATA_ALL, "amount");
+	var sizeMinMax = getMinMax(URL_DATA_ALL, "size_all");
+	DATA_ALL["pageDataGeneral"] = {}
+	DATA_ALL["pageDataGeneral"]["amountMin"] = amountMinMax[0];
+	DATA_ALL["pageDataGeneral"]["amountMax"] = amountMinMax[1];
+	DATA_ALL["pageDataGeneral"]["sizeMin"] = sizeMinMax[0];
+	DATA_ALL["pageDataGeneral"]["sizeMax"] = sizeMinMax[1];
+	DATA_ALL["pageDataAll"] = URL_DATA_ALL
 
-module.exports = main;
+	DATA_ALL = JSON.stringify(DATA_ALL, null, '\t');
+	fs.write(TARGET_FILE, DATA_ALL, 'w');
+	casper.exit();
+}
+delayedLoop(URL_ALL.length-1);
+
